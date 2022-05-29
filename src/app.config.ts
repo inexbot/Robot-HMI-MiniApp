9868c06b7b9eb5e455713ab521060f887b4ea4c4
MintOTea <weifan93@outlook.com>
2022-05-29 15:44:01 +0800

    a

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,10 +1,9 @@
 export default defineAppConfig({
-  // pages: ["pages/index/index", "pages/teach/index", "pages/teach/jog/index"],
-  pages: ["pages/teach/index", "pages/teach/jog/index"],
+  pages: ["pages/index/index", "pages/teach/index", "pages/teach/jog/index"],
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
-    navigationBarTitleText: "WeChat",
+    navigationBarTitleText: "纳博特科技",
     navigationBarTextStyle: "black",
   },
 });
